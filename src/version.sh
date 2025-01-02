dffa457da985f853b4b011ea71c1f5ab3c92b60d
foudil <username@users.noreply.github.com>
2025-01-02 16:51:36 +0100

    Bump version

--- src/version.sh
+++ src/version.sh
@@ -1,4 +1,4 @@
 # git-post-commit
 # git diff --cached --name-status | grep -q -E 'M src/version.sh'
 # if yes then git tag "v${SJAIL_VERSION}"
-SJAIL_VERSION="v0.1.0-9-gfcf7571"
+SJAIL_VERSION="v0.2.0"
